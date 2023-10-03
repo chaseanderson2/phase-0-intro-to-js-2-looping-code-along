7e4975ef1da7ff1632b78df202e4d69a275f3818
Chase Anderson <chaseanderson.stu@gmail.com>
2023-10-03 14:07:36 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, eventName)   {
+    let messages = [];
+    
+    for (let i = 0; i< names.length; i++)   {
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+function countDown(num)    {
+    let currentNumber = num;
+
+    while (currentNumber >= 0)  {
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
+
+countDown(10);
